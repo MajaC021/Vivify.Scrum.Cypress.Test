d9a1f7896f53a1ebaeecc1d25d000f1a91c07ef6
MajaC021 <majacveticanin90@gmail.com>
2022-06-09 12:49:39 +0200

    add intercept in task

--- cypress/integration/tasks.spec.js
+++ cypress/integration/tasks.spec.js
@@ -2,10 +2,15 @@ import dataTask from "../fixtures/data.json"
 import Login from '../support/classes/login';
 import Logout from '../support/classes/logout';
 import Tasks from '../support/classes/tasks';
+import Boards from '../support/classes/boards';
 
 const login = new Login();
 const logout = new Logout();
 const tasks = new Tasks();
+const boards = new Boards();
+let orgId;
+let code;
+let boardId;
 
 describe("tasks module tests", () => {
 
@@ -14,49 +19,71 @@ describe("tasks module tests", () => {
             method: "POST",
             url: "api/v2/login",
         }).as('login')
-        login.login(dataOrg.user.email, dataOrg.user.pass)
+        login.login(dataTask.user.email, dataTask.user.pass)
         login.assertLogin();
         cy.wait('@login').then((interceptObj) => {
-            user = interceptObj.response.body.user.full_name;
             expect(interceptObj.response.statusCode).eq(200)
-            expect(interceptObj.response.body.user.email).eq(dataOrg.user.email)
+            expect(interceptObj.response.body.user.email).eq(dataTask.user.email)
         })
     });
 
-    afterEach("logout user", () => {
-        cy.intercept({
-            method: "POST",
-            url: "api/v2/logout",
-          }).as('logout')
-          logout.logout("Maja Cvet");
-          logout.assertLogout();
-          cy.wait('@logout').then((interceptObj) => {
-            console.log(interceptObj)
-            expect(interceptObj.response.statusCode).eq(201)
-            expect(interceptObj.response.body.message).eq("Successfully logged out")
-          })
-    })
+    // afterEach("logout user", () => {
+    //     cy.intercept({
+    //         method: "POST",
+    //         url: "api/v2/logout",
+    //     }).as('logout')
+    //     logout.logout("Maja Cvet");
+    //     logout.assertLogout();
+    //     cy.wait('@logout').then((interceptObj) => {
+    //         expect(interceptObj.response.statusCode).eq(201)
+    //         expect(interceptObj.response.body.message).eq("Successfully logged out")
+    //     })
+    // })
 
     //positive
     it("Create new task", () => {
+        boards.createBoard("test12345")
+        cy.intercept({
+            method: "POST",
+            url: "api/v2/tasks",
+        }).as('createTaskRequest')
         tasks.createTask(dataTask.task.tskName)
         tasks.assertCreatedTask(dataTask.task.tskName)
+        cy.wait('@createTaskRequest').then((interceptObj) => {           
+            orgId = interceptObj.response.body.id
+            code = interceptObj.response.body.code;
+            boardId = interceptObj.response.body.board.id
+            expect(interceptObj.response.statusCode).eq(201)
+            expect(interceptObj.response.body.name).eq(dataTask.task.tskName)
+        })
     })
 
-    it("Delete the task", () => {
-        tasks.deleteTask()
-        tasks.assertDeletedTask()
-    })
     it("Open task in new tab", () => {
         tasks.moveTaskToOtherBoard()
         tasks.assertMoveTaskToOtherBoard()
     })
+    
     it("upload file in task", () => {
         tasks.uploadFile("../images/Screenshot_1.png")
         tasks.assertUploadedFiles("Screenshot_1.png")
     })
 
-    //negative
+    it("Delete the task", () => {
+        cy.visit(`/boards/${boardId}/${code}`)
+        cy.intercept({
+            method: "DELETE",
+            url: `api/v2/tasks/${orgId}?boardId=${boardId}`,
+        }).as('deleteTaskRequest')
+        tasks.deleteTask()
+        tasks.assertDeletedTask()
+        cy.wait('@deleteTaskRequest').then((interceptObj) => {
+            console.log(interceptObj)
+            expect(interceptObj.response.statusCode).eq(200)
+            expect(interceptObj.response.body.name).eq(dataTask.task.tskName)
+        })
+    })
+
+    // //negative
     it("Edit task without name", () => {
         tasks.editTaskNameWithoutName()
         tasks.assertEditTaskNameWithoutName()
--- cypress/support/classes/tasks/index.js
+++ cypress/support/classes/tasks/index.js
@@ -3,7 +3,6 @@ import TaskElements from '../../elements/task-elements';
 class Tasks {
 
     createTask(taskName) {
-        cy.get(TaskElements.openBoard).eq(0).click()
         cy.get(TaskElements.addNewTask).eq(1).click({ force: true })
         cy.get(TaskElements.addNameTask).type(`${taskName}{enter}`)
     }
@@ -16,8 +15,6 @@ class Tasks {
         })
     }
     deleteTask() {
-        cy.get(TaskElements.openBoard).eq(0).click()
-        cy.get(TaskElements.listTask).eq(1).click()
         cy.get(TaskElements.dropdownTskOptions).eq(3).click()
         cy.get(TaskElements.deleteTask).click()
         cy.get(TaskElements.saveConfirmBtn).click()
@@ -35,8 +32,8 @@ class Tasks {
         cy.get('button[name="update_item_title"]').should('not.have.css', 'display', 'none')
     }
     moveTaskToOtherBoard() {
-        cy.get(TaskElements.openBoard).eq(0).click()
-        cy.get(TaskElements.listTask).eq(1).click()
+        // cy.get(TaskElements.openBoard).eq(0).click()
+        // cy.get(TaskElements.listTask).eq(1).click()
         cy.get(TaskElements.dropdownTskOptions).eq(3).click()
         cy.get(TaskElements.itemsFromDropdown).contains("Move To Another Board").click()
         cy.get(TaskElements.selectBoardDropdown).click()
